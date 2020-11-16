59e49186a0ed077e2155c01f1a7ded67330452c3
Sander van de Velde <sander@vandevelde-online.com>
2020-11-16 16:08:31 +0100

    write

--- src/Program.cs
+++ src/Program.cs
@@ -14,9 +14,14 @@ namespace ExpanderConsoleApp
             var connectionSettingsx20 = new I2cConnectionSettings(1, 0x20);
             var i2cDevicex20 = I2cDevice.Create(connectionSettingsx20);
             var mcp23017x20 = new Mcp23017(i2cDevicex20);
-
             mcp23017x20.WriteByte(Register.IODIR, 0b0000_0000, Port.PortA);
             mcp23017x20.WriteByte(Register.IODIR, 0b0000_0000, Port.PortB);
+
+            var connectionSettingsx24 = new I2cConnectionSettings(1, 0x24);
+            var i2cDevicex24 = I2cDevice.Create(connectionSettingsx24);
+            var mcp23017x24 = new Mcp23017(i2cDevicex24);
+            mcp23017x24.WriteByte(Register.IODIR, 0b0000_0000, Port.PortA);
+            mcp23017x24.WriteByte(Register.IODIR, 0b0000_0000, Port.PortB);
 
 
             while (true)
@@ -26,6 +31,9 @@ namespace ExpanderConsoleApp
 
                 Console.WriteLine($"Port A = {dataPortAswitch20:D3} - Port B = {dataPortBswitch20:D3}");
 
+                mcp23017x24.WriteByte(Register.GPIO, 128, Port.PortA);
+                mcp23017x24.WriteByte(Register.GPIO, 1, Port.PortB);
+                   
                 Task.Delay(5000).Wait();
             }
         }
