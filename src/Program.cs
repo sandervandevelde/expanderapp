106bcf0f86b588aa32f91412f7030885d033bc73
Sander van de Velde <sander@vandevelde-online.com>
2020-11-16 14:37:45 +0100

    initial version

---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using Iot.Device.Mcp23xxx;
+using System;
+using System.Device.I2c;
+using System.Threading.Tasks;
+
+namespace ExpanderConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var connectionSettingsx20 = new I2cConnectionSettings(1, 0x20);
+            var i2cDevicex20 = I2cDevice.Create(connectionSettingsx20);
+            var mcp23017x20 = new Mcp23017(i2cDevicex20);
+
+            var connectionSettingsx24 = new I2cConnectionSettings(1, 0x24);
+            var i2cDevicex24 = I2cDevice.Create(connectionSettingsx24);
+            var mcp23017x24 = new Mcp23017(i2cDevicex24);
+
+            while (true)
+            {
+                byte dataPortAswitch = mcp23017x20.ReadByte(Register.GPIO, Port.PortA);
+                byte dataPortBswitch = mcp23017x20.ReadByte(Register.GPIO, Port.PortB);
+
+                Console.WriteLine($"Port A = {dataPortAswitch:D3} - Port B = {dataPortBswitch:D3}");
+
+                Task.Delay(200);
+            }
+        }
+    }
+}
